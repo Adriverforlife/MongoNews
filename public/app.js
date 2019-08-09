d489bbdcdc5f9703235500cf65f574d36ed786b3
Michael Eguina <michael@corner-exit.com>
2019-08-08 21:27:40 -0700

    added handlebars. broke stuff

--- public/app.js
+++ public/app.js
@@ -3,13 +3,13 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<h2 class='title' data-id='" + data[i]._id + "'>" + data[i].title + "</h2><p><br />" + data[i].link + "</p>");
   }
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", ".title", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -70,3 +70,12 @@ $(document).on("click", "#savenote", function() {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
+
+$(document).on("click","#scrapeNews", function() {
+  console.log("clicked")
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  })
+})
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require("express");
+var router = express.Router();
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var app = express();
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -14,7 +22,6 @@ var db = require("./models");
 var PORT = 3000;
 
 // Initialize Express
-var app = express();
 
 // Configure middleware
 
@@ -27,37 +34,43 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/newsScrapper", { useNewUrlParser: true });
 
 // Routes
+app.get("/", function(req, res) {
+ 
+   // console.log(hbsObject);
+    res.render("index", );
+  });
+
+
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.lifehacker.com").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $(".js_link").each(function (i, element) {
       // Save an empty result object
       var result = {};
-
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+        .find("h1")
         .text();
       result.link = $(this)
-        .children("a")
+        // .find("a")
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, log it
           console.log(err);
         });
@@ -69,59 +82,59 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   // TODO: Finish the route so it grabs all of the articles
   db.Article.find({})
-  .then(function(article) {
+  .then(function(dbArticle) {
     // If all Users are successfully found, send them back to the client
-    res.json(article);
+    res.json(dbArticle);
   })
-  .catch(function(err) {
-    // If an error occurs, send the error back to the client
-    res.json(err);
-  });
+    .catch(function (err) {
+      // If an error occurs, send the error back to the client
+      res.json(err);
+    });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   // TODO
   // ====
   // Finish the route so it finds one article using the req.params.id,
   // and run the populate method with "note",
   // then responds with the article with the note included
-  db.Article.find({ _id: req.params.id})
-  .populate("note")
-    .then(function(dbArticle) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (dbArticle) {
       // If all Users are successfully found, send them back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurs, send the error back to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // TODO
   // ====
   // save the new note that gets posted to the Notes collection
   // then find an article from the req.params.id
   // and update it's "note" property with the _id of the new note
   db.Note.create(req.body)
-  .then(function(dbNote) {
-    
-    return db.Article.findOneAndUpdate({_id:req.params.id}, { notes: dbNote._id }, { new: true });
-  })
-  .then(function(dbUser) {
-    res.json(dbUser);
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
+    .then(function (dbNote) {
+
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { notes: dbNote._id }, { new: true });
+    })
+    .then(function (dbUser) {
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
